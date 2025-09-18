3183e905b6b00354815345b523cdfced8b5d2453
keerthikaa27 <keerthikanimmagadda@gmail.com>
2025-09-18 23:08:29 +0530

    Fix CORS origin in index.js

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://news-chatbot-frontend-e592.onrender.com/', 'http://localhost:3000'],
+  credentials: true
+}));
+
 app.use(bodyParser.json());
 
 // Redis client
